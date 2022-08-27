9ad080e0e6ea0d4f3955d0c3832678cdda802c7f
askia318 <ccli.mail@gmail.com>
2022-08-27 15:43:02 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -18,12 +18,35 @@ from PIL import Image
 from pickle import load
 
 @st.cache
-def classify_fault(model, df):
+def classify_fault(model, df, encoder):
     temp = model.predict(df)
     pred_y = np.argmax(temp, axis=1)
-    encoder = LabelEncoder()
+    #encoder = LabelEncoder()
     pred_class_y = encoder.inverse_transform(pred_y)
     return pred_class_y
+
+@st.cache
+def load_data(select_system):
+    if select_system == 'a':
+        st.write('The expected accuracy is 83.4%')
+        data = pd.read_csv("minmax_a.csv")
+    if select_system == 'b':
+        st.write('The expected accuracy is 75.64%')
+        data = pd.read_csv("minmax_b.csv")
+    return data
+
+# load the model and scaler
+@st.cache
+def load_selected_model(select_system):
+    if select_system == 'a':
+        model = load(open('model_a.pkl', 'rb'))
+        scaler = load(open('scaler_a.pkl', 'rb'))
+        encoder = load(open('encoder_a.pkl', 'rb'))
+    if select_system == 'b':
+        model = load(open('model_b.pkl', 'rb'))
+        scaler = load(open('scaler_b.pkl', 'rb'))
+        encoder = load(open('encoder_b.pkl', 'rb'))
+    return model, scaler, encoder
 
 
 # this is the main function in which we define our webpage
@@ -37,7 +60,7 @@ def main():
     st.header('By TPRI HV Lab Dr. Cheng-Chung Li, Shuo-Fu Hong, Wei-Chih Liang')
 
     col1, col2 = st.columns(2)
-    col1.write('The models are built by the simulation data generated from RTDS,\
+    col1.write('The DL models are built by the simulation data generated from RTDS,\
                 and their goal is to classify \
                 the expected fault type of a transmission line.')
 
@@ -51,33 +74,36 @@ def main():
     system_list = ['a', 'b']
     select_system = st.radio("Pick one system to classify", system_list)
 
+    data = load_data(select_system)
     # following lines create boxes in which user can enter data required to make prediction
 
+    initial = (float(data['Min'].loc[0]) + float(data['Max'].loc[0]))/2
+
     st.subheader('Input Features')
-    IAang = st.slider('IAang', float(data['Min'].loc[0]), float(data['Max'].loc[0]), -9.762292, step = 0.01)
-    IAang2 = st.slider('IAang2', float(data['Min'].loc[1]), float(data['Max'].loc[1]), 170.201531, step = 0.01)
-    IAmag = st.slider('IAmag', float(data['Min'].loc[2]), float(data['Max'].loc[2]), 0.219938, step = 0.01)
-    IAmag2 = st.slider('IAmag2', float(data['Min'].loc[3]), float(data['Max'].loc[3]), 0.219956, step = 0.01)
-    IBang = st.slider('IBang', float(data['Min'].loc[4]), float(data['Max'].loc[4]), -129.734629, step = 0.01)
-    IBang2 = st.slider('IBang2', float(data['Min'].loc[5]), float(data['Max'].loc[5]), 50.229159, step = 0.01)
-    IBmag = st.slider('IBmag', float(data['Min'].loc[6]), float(data['Max'].loc[6]), 0.219811, step = 0.01)
-    IBmag2 = st.slider('IBmag2', float(data['Min'].loc[7]), float(data['Max'].loc[7]), 0.219829, step = 0.01)
-    ICang = st.slider('ICang', float(data['Min'].loc[8]), float(data['Max'].loc[8]), 110.280106, step = 0.01)
-    ICang2 = st.slider('ICang2', float(data['Min'].loc[9]), float(data['Max'].loc[9]), -69.756059, step = 0.01)
-    ICmag = st.slider('ICmag', float(data['Min'].loc[10]), float(data['Max'].loc[10]), 0.219967, step = 0.01)
-    ICmag2 = st.slider('ICmag2', float(data['Min'].loc[11]), float(data['Max'].loc[11]), 0.219985, step = 0.01)
-    VAang1 = st.slider('VAang1', float(data['Min'].loc[12]), float(data['Max'].loc[12]), -1.187182, step = 0.01)
-    VAang2 = st.slider('VAang2', float(data['Min'].loc[13]), float(data['Max'].loc[13]), -1.189900, step = 0.01)
-    VAmag1 = st.slider('VAmag1', float(data['Min'].loc[14]), float(data['Max'].loc[14]), 181.429819, step = 0.01)
-    VAmag2 = st.slider('VAmag2', float(data['Min'].loc[15]), float(data['Max'].loc[15]), 181.383600, step = 0.01)
-    VBang1 = st.slider('VBang1', float(data['Min'].loc[16]), float(data['Max'].loc[16]), -121.155129, step = 0.01)
-    VBang2 = st.slider('VBang2', float(data['Min'].loc[17]), float(data['Max'].loc[17]), -121.157849, step = 0.01)
-    VBmag1 = st.slider('VBmag1', float(data['Min'].loc[18]), float(data['Max'].loc[18]), 181.383244, step = 0.01)
-    VBmag2 = st.slider('VBmag2', float(data['Min'].loc[19]), float(data['Max'].loc[19]), 181.337034, step = 0.01)
-    VCang1 = st.slider('VCang1', float(data['Min'].loc[20]), float(data['Max'].loc[20]), 118.841576, step = 0.01)
-    VCang2 = st.slider('VCang2', float(data['Min'].loc[21]), float(data['Max'].loc[21]), 118.838858, step = 0.01)
-    VCmag1 = st.slider('VCmag1', float(data['Min'].loc[22]), float(data['Max'].loc[22]), 181.494419, step = 0.01)
-    VCmag2 = st.slider('VCmag2',float(data['Min'].loc[23]), float(data['Max'].loc[23]), 181.448174, step = 0.01)
+    IAang = st.slider('IAang', float(data['Min'].loc[0]), float(data['Max'].loc[0]), initial, step = 0.01)
+    IAang2 = st.slider('IAang2', float(data['Min'].loc[1]), float(data['Max'].loc[1]), initial, step = 0.01)
+    IAmag = st.slider('IAmag', float(data['Min'].loc[2]), float(data['Max'].loc[2]), initial, step = 0.01)
+    IAmag2 = st.slider('IAmag2', float(data['Min'].loc[3]), float(data['Max'].loc[3]), initial, step = 0.01)
+    IBang = st.slider('IBang', float(data['Min'].loc[4]), float(data['Max'].loc[4]), initial, step = 0.01)
+    IBang2 = st.slider('IBang2', float(data['Min'].loc[5]), float(data['Max'].loc[5]), initial, step = 0.01)
+    IBmag = st.slider('IBmag', float(data['Min'].loc[6]), float(data['Max'].loc[6]), initial, step = 0.01)
+    IBmag2 = st.slider('IBmag2', float(data['Min'].loc[7]), float(data['Max'].loc[7]), initial, step = 0.01)
+    ICang = st.slider('ICang', float(data['Min'].loc[8]), float(data['Max'].loc[8]), initial, step = 0.01)
+    ICang2 = st.slider('ICang2', float(data['Min'].loc[9]), float(data['Max'].loc[9]), initial, step = 0.01)
+    ICmag = st.slider('ICmag', float(data['Min'].loc[10]), float(data['Max'].loc[10]), initial, step = 0.01)
+    ICmag2 = st.slider('ICmag2', float(data['Min'].loc[11]), float(data['Max'].loc[11]), initial, step = 0.01)
+    VAang1 = st.slider('VAang1', float(data['Min'].loc[12]), float(data['Max'].loc[12]), initial, step = 0.01)
+    VAang2 = st.slider('VAang2', float(data['Min'].loc[13]), float(data['Max'].loc[13]), initial, step = 0.01)
+    VAmag1 = st.slider('VAmag1', float(data['Min'].loc[14]), float(data['Max'].loc[14]), initial, step = 0.01)
+    VAmag2 = st.slider('VAmag2', float(data['Min'].loc[15]), float(data['Max'].loc[15]), initial, step = 0.01)
+    VBang1 = st.slider('VBang1', float(data['Min'].loc[16]), float(data['Max'].loc[16]), initial, step = 0.01)
+    VBang2 = st.slider('VBang2', float(data['Min'].loc[17]), float(data['Max'].loc[17]), initial, step = 0.01)
+    VBmag1 = st.slider('VBmag1', float(data['Min'].loc[18]), float(data['Max'].loc[18]), initial, step = 0.01)
+    VBmag2 = st.slider('VBmag2', float(data['Min'].loc[19]), float(data['Max'].loc[19]), initial, step = 0.01)
+    VCang1 = st.slider('VCang1', float(data['Min'].loc[20]), float(data['Max'].loc[20]), initial, step = 0.01)
+    VCang2 = st.slider('VCang2', float(data['Min'].loc[21]), float(data['Max'].loc[21]), initial, step = 0.01)
+    VCmag1 = st.slider('VCmag1', float(data['Min'].loc[22]), float(data['Max'].loc[22]), initial, step = 0.01)
+    VCmag2 = st.slider('VCmag2',float(data['Min'].loc[23]), float(data['Max'].loc[23]), initial, step = 0.01)
 
     features = {'IAang': IAang, 'IAang2': IAang2,'IAmag': IAmag, 'IAmag2': IAmag2,
                 'IBang': IBang, 'IBang2': IBang2, 'IBmag': IBmag, 'IBmag2': IBmag2,
@@ -104,19 +130,13 @@ def main():
     st.caption('Voltage Features')
     st.dataframe(features_df_V)
 
-    # load the model
-    @st.cache
-    model = load(open('model.pkl', 'rb'))
-    # load the scaler
-    st.cache
-    scaler = load(open('scaler.pkl', 'rb'))
-
-    feature_df = scaler.transform(features_df)
+    model, scaler, encoder = load_selected_model(select_system)
+    feature_df_scaled = scaler.transform(features_df)
 
     # when 'Predict' is clicked, make the prediction and store it
     if st.button("Predict"):
         fault_type = ''
-        prediction = classify_fault(model, features_df)
+        prediction = classify_fault(model, features_df_scaled, encoder)
 
         if str(prediction) == 'AG':
             fault_type = 'AG'
